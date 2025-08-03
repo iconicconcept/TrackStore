eaeae3d50118085335ea5ed3b1914e72870390f3
Mubaraq Allamalyekeen <mubaraqadeniyi159@gmail.com>
2025-08-04 00:51:13 +0100

    psuh

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:7000/api" : "/api",
+    baseURL: import.meta.mode === "development" ? "http://localhost:7000/api" : "https://trackstore.onrender.com/api",
     withCredentials: true //allows to send cookies to the server
 })
 
